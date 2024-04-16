88b0c3c13fc540739b3eec073a1a7a3e33d79225
Upadhyay B <upadhyay.b@dgsl.in>
2024-04-16 20:54:31 +0530

    complete 2

--- src/pages/user/change-password.js
+++ src/pages/user/change-password.js
@@ -1,13 +1,10 @@
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import Cookies from "js-cookie";
-
-//internal import
-import Error from "@component/form/Error";
-import Dashboard from "@pages/user/dashboard";
-import InputArea from "@component/form/InputArea";
-import UserServices from "@services/UserServices";
-import { notifyError, notifySuccess } from "@utils/toast";
+import Error from "@/components/form/Error";
+import InputArea from "@/components/form/InputArea";
+import UserServices from "@/services/UserServices";
+import { notifyError, notifySuccess } from "@/utils/toast";
 
 const ChangePassword = () => {
   const {
@@ -35,60 +32,57 @@ const ChangePassword = () => {
   });
 
   return (
-    <Dashboard
-      title="Change-Password"
-      description="This is change-password page"
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col min-w-[500px] w-full h-full items-center justify-center"
     >
-      <h2 className="text-xl font-serif font-semibold mb-5">Change Password</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="md:grid-cols-6 md:gap-6">
-          <div className="md:mt-0 md:col-span-2">
-            <div className="lg:mt-6 bg-white">
-              <div className="grid grid-cols-6 gap-6">
-                <div className="col-span-6 sm:col-span-6">
-                  <InputArea
-                    register={register}
-                    label="Email Address"
-                    name="email"
-                    type="email"
-                    placeholder="Your Email"
-                  />
-                  <Error errorName={errors.email} />
-                </div>
-                <div className="col-span-6 sm:col-span-6">
-                  <InputArea
-                    register={register}
-                    label="Current Password"
-                    name="currentPassword"
-                    type="password"
-                    placeholder="Your Current Password"
-                  />
-                  <Error errorName={errors.currentPassword} />
-                </div>
-                <div className="col-span-6 sm:col-span-6">
-                  <InputArea
-                    register={register}
-                    label="New Password"
-                    name="newPassword"
-                    type="password"
-                    placeholder="Your New Password"
-                  />
-                  <Error errorName={errors.newPassword} />
-                </div>
+      <div className="md:grid-cols-6 md:gap-6">
+        <div className="md:mt-0 md:col-span-2">
+          <div className="lg:mt-6 bg-white">
+            <div className="grid grid-cols-6 gap-6">
+              <div className="col-span-6 sm:col-span-6">
+                <InputArea
+                  register={register}
+                  name="email"
+                  type="email"
+                  placeholder="Your Email"
+                  className="p-2 border border-[#770006] w-full -mt-2"
+                />
+                <Error errorName={errors.email} />
+              </div>
+              <div className="col-span-6 sm:col-span-6">
+                <InputArea
+                  register={register}
+                  name="currentPassword"
+                  type="password"
+                  placeholder="Your Current Password"
+                  className="p-2 border border-[#770006] w-full -mt-2"
+                />
+                <Error errorName={errors.currentPassword} />
+              </div>
+              <div className="col-span-6 sm:col-span-6">
+                <InputArea
+                  register={register}
+                  name="newPassword"
+                  type="password"
+                  placeholder="Your New Password"
+                  className="p-2 border border-[#770006] w-full -mt-2"
+                />
+                <Error errorName={errors.newPassword} />
               </div>
             </div>
           </div>
         </div>
-        <div className="mt-5 text-right">
-          <button
-            type="submit"
-            className="md:text-sm leading-5 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-medium text-center justify-center border-0 border-transparent rounded-md placeholder-white focus-visible:outline-none focus:outline-none new-bg text-white px-5 md:px-6 lg:px-8 py-2 md:py-3 lg:py-3 hover:text-white hover:bg-emerald-600 h-12 mt-1 text-sm lg:text-sm w-full sm:w-auto"
-          >
-            Change Password
-          </button>
-        </div>
-      </form>
-    </Dashboard>
+      </div>
+      <div className="mt-5 text-right">
+        <button
+          type="submit"
+          className=" text-center py-3 font-medium text-sm text-white transition-all focus:outline-none bg-[#770006] my-1 w-[200px]"
+        >
+          Change Password
+        </button>
+      </div>
+    </form>
   );
 };
 
